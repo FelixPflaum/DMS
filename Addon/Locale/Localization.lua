8032a5c7a689c14c93e7e7803f6108e87f9ed3ad
FelixPflaum <141590183+FelixPflaum@users.noreply.github.com>
2024-08-11 22:54:24 +0200

    locale file creator

--- Addon/Locale/Localization.lua
+++ Addon/Locale/Localization.lua
@@ -1,23 +1,25 @@
 ---@class AddonEnv
-local Env = select(2, ...);
+local Env = select(2, ...)
 
-local clientlocale = GetLocale();
+local clientlocale = GetLocale()
 ---@type table<string, string>
-local localStrings = setmetatable({}, {__index=function(self, key)
-    rawset(self, key, key);
-    return key;
-end});
+local localStrings = setmetatable({}, {
+    __index = function(self, key)
+        rawset(self, key, key)
+        return key
+    end
+})
 
 --- Add localization
 ---@param locale string
 function Env:AddLocalization(locale)
-	if locale ~= clientlocale then
-		return;
-	end
-	return localStrings;
+    if locale ~= clientlocale then
+        return
+    end
+    return localStrings
 end
 
 --- Get localization table
 function Env:GetLocalization()
-	return localStrings;
+    return localStrings
 end
---
+++ Tools/LocaleUpdater/localeFileCreator.ts
@@ -0,0 +1,59 @@
+import { readdirSync, readFileSync, statSync, writeFileSync } from "fs";
+import { join } from "path";
+
+const ADDON_DIR = __dirname + "/../../Addon";
+
+const ignoredDirs: { [dirName: string]: boolean } = {
+    Libs: true,
+    _dev: true,
+    Locale: true,
+}
+
+function scanForLtags(filePath: string, list: { [str: string]: boolean }) {
+    const content = readFileSync(filePath, "utf-8");
+    const matches = content.match(/L\["(.*?)"\]/g);
+    if (matches) {
+        for (const match of matches) {
+            let str = match.substring(3, match.length - 2);
+            list[str] = true;
+        }
+    }
+}
+
+function getLuaFileList(dir: string, list?: string[]) {
+    list = list ?? [];
+    const fileList = readdirSync(dir);
+    for (const file of fileList) {
+        const pathToFile = join(dir, file);
+        if (statSync(pathToFile).isDirectory()) {
+            if (!ignoredDirs[file]) getLuaFileList(pathToFile, list);
+        } else if (file.split(".").pop() == "lua") {
+            list.push(pathToFile);
+        }
+    }
+    return list;
+}
+
+const files = getLuaFileList(ADDON_DIR);
+
+const allStrings: { [str: string]: boolean } = {};
+for (const file of files) {
+    scanForLtags(file, allStrings);
+}
+
+let lua = `--------------------------------------------------------------------
+--- Generated file, do not edit.
+--------------------------------------------------------------------
+
+---@class AddonEnv
+local Env = select(2, ...)
+
+local L = Env:AddLocalization("enUS")
+`;
+
+for (const str in allStrings) {
+    lua += `L["${str}"] = "${str}"\n`;
+}
+
+writeFileSync(ADDON_DIR + "/Locale/enUS.lua", lua);
+console.log("Wrote enUS.lua.");
