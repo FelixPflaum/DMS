1926e33a5011fc0ea5a63f32f639bf7485dd9625
FelixPflaum <141590183+FelixPflaum@users.noreply.github.com>
2024-09-17 12:03:34 +0200

    Make events stored as key to force uniqueness

--- Addon/System/Events.lua
+++ Addon/System/Events.lua
@@ -4,14 +4,13 @@ local ADDON_NAME = select(1, ...)
 local Env = select(2, ...)
 
 local eventFrame = CreateFrame("Frame")
----@type table<string, (table|fun(...))[]>
+---@type table<string, {handlers:table<(table|fun(...)),boolean>, count:integer}>
 local eventHandlers = {}
 
 eventFrame:SetScript("OnEvent", function(_, event, ...)
     if eventHandlers[event] then
-        local handlers = eventHandlers[event]
-        for i = #handlers, 1, -1 do
-            local funcOrObj = handlers[i]
+        local eventTable = eventHandlers[event]
+        for funcOrObj in pairs(eventTable.handlers) do
             if type(funcOrObj) == "table" then
                 funcOrObj[event](funcOrObj, ...)
             else
@@ -25,13 +24,16 @@ end)
 ---@param event string
 ---@param callbackOrObject table|fun(...:any):boolean|nil If table then the function table:EVENT(args) will be called.
 function Env:RegisterEvent(event, callbackOrObject)
-    eventHandlers[event] = eventHandlers[event] or {}
-    table.insert(eventHandlers[event], callbackOrObject)
-    if #eventHandlers[event] == 1 then
-        eventFrame:RegisterEvent(event)
-        self:PrintDebug("Registered game event", event)
+    eventHandlers[event] = eventHandlers[event] or { handlers = {}, count = 0 }
+    if not eventHandlers[event].handlers[callbackOrObject] then
+        eventHandlers[event].handlers[callbackOrObject] = true
+        eventHandlers[event].count = eventHandlers[event].count + 1
+        if eventHandlers[event].count == 1 then
+            eventFrame:RegisterEvent(event)
+            self:PrintDebug("Registered game event", event)
+        end
+        self:PrintDebug("Added event callback", event, callbackOrObject)
     end
-    self:PrintDebug("Added event callback", event, callbackOrObject)
 end
 
 ---Remove event callback.
@@ -39,12 +41,12 @@ end
 ---@param callbackOrObject table|fun(...:any):boolean|nil
 function Env:UnregisterEvent(event, callbackOrObject)
     if not eventHandlers[event] then return end
-    for i = #eventHandlers[event], 1, -1 do
-        local funcOrObj = eventHandlers[event][i]
+    for funcOrObj in pairs(eventHandlers[event].handlers) do
         if funcOrObj == callbackOrObject then
-            table.remove(eventHandlers[event], i)
+            eventHandlers[event].handlers[funcOrObj] = nil
+            eventHandlers[event].count = eventHandlers[event].count - 1
             self:PrintDebug("Removed event callback", event, callbackOrObject)
-            if #eventHandlers[event] == 0 then
+            if eventHandlers[event].count == 0 then
                 eventFrame:UnregisterEvent(event)
                 self:PrintDebug("Unregistered game event", event)
             end
