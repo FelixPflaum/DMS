a89105aecfcea7c05f244b8a4760604f18141346
FelixPflaum <141590183+FelixPflaum@users.noreply.github.com>
2024-11-13 11:24:08 +0100

    kek

--- Addon/Modules/Meow.lua
+++ Addon/Modules/Meow.lua
@@ -15,7 +15,7 @@ local meows = { "Meow.mp3", "Meow2.mp3" }
 Env:RegisterEvent("CHAT_MSG_TEXT_EMOTE", function(text, sender)
     ---@cast text string
     ---@cast sender string
-    if text:find("meow") then
+    if text:find("meow") or text:find("miau") then
         local _, class = UnitClass(sender)
         if class == "DRUID" then
             local catform = GetSpellInfo(768)
